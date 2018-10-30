b51841710a64a8002dedc8a3218f0ab86a2dfd75
Benjamin Beuscart <benjamin.beuscart@isen.yncrea.fr>
2018-10-30 18:47:52 +0100

    modif script création database

--- Script création base de données/databaseprojet.sql
+++ Script création base de données/databaseprojet.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 26 oct. 2018 à 13:28
--- Version du serveur :  5.7.21
--- Version de PHP :  5.6.35
+-- Généré le :  mar. 30 oct. 2018 à 17:46
+-- Version du serveur :  5.7.23
+-- Version de PHP :  7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,13 +32,9 @@ DROP TABLE IF EXISTS `orders`;
 CREATE TABLE IF NOT EXISTS `orders` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` int(10) UNSIGNED DEFAULT NULL,
-  `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `status` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `amount` double DEFAULT NULL,
   `billing_adress_id` int(10) UNSIGNED DEFAULT NULL,
   `delivery_adress_id` int(10) UNSIGNED DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `IDX_user_billing_adress` (`billing_adress_id`),
   KEY `IDX_user_delivery_adress` (`delivery_adress_id`),
@@ -49,8 +45,8 @@ CREATE TABLE IF NOT EXISTS `orders` (
 -- Déchargement des données de la table `orders`
 --
 
-INSERT INTO `orders` (`id`, `user_id`, `type`, `status`, `amount`, `billing_adress_id`, `delivery_adress_id`, `created_at`, `updated_at`) VALUES
-(2, 1, 'CART', 'CART', 149.52, 1, 2, '2018-10-26 14:10:28', '2018-10-26 14:10:28');
+INSERT INTO `orders` (`id`, `user_id`, `amount`, `billing_adress_id`, `delivery_adress_id`) VALUES
+(2, 1, 149.52, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -78,9 +74,7 @@ CREATE TABLE IF NOT EXISTS `order_addresses` (
 
 INSERT INTO `order_addresses` (`id`, `human_name`, `address_one`, `address_two`, `postal_code`, `city`, `country`, `created_at`, `updated_at`) VALUES
 (2, 'Fred Eric', '2 impasse Duvet', '3ieme étage', '59000', 'Lille', 'FRANCE', '2018-10-26 14:10:28', '2018-10-26 14:10:28'),
-(3, 'Fred Eric', '120 Boulevard Vaubant', 'B506', '59000', 'Lille', 'FRANCE', '2018-10-26 14:10:28', '2018-10-26 14:10:28'),
-(4, 'Fred Eric', '2 impasse Duvet', '3ieme étage', '59000', 'Lille', 'FRANCE', '2018-10-26 14:10:28', '2018-10-26 14:10:28'),
-(5, 'Fred Eric', '120 Boulevard Vaubant', 'B506', '59000', 'Lille', 'FRANCE', '2018-10-26 14:10:28', '2018-10-26 14:10:28');
+(3, 'Fred Eric', '120 Boulevard Vaubant', 'B506', '59000', 'Lille', 'FRANCE', '2018-10-26 14:10:28', '2018-10-26 14:10:28');
 
 -- --------------------------------------------------------
 
@@ -95,8 +89,6 @@ CREATE TABLE IF NOT EXISTS `order_products` (
   `product_id` int(10) UNSIGNED DEFAULT NULL,
   `quantity` int(3) UNSIGNED NOT NULL,
   `unit_price` double DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `IDX_order_product` (`order_id`),
   KEY `IDX_product_order` (`product_id`)
@@ -106,10 +98,10 @@ CREATE TABLE IF NOT EXISTS `order_products` (
 -- Déchargement des données de la table `order_products`
 --
 
-INSERT INTO `order_products` (`id`, `order_id`, `product_id`, `quantity`, `unit_price`, `created_at`, `updated_at`) VALUES
-(2, 1, 1, 1, 57.08, '2018-10-26 14:10:28', '2018-10-26 14:10:28'),
-(3, 1, 2, 3, 46.22, '2018-10-26 14:10:28', '2018-10-26 14:10:28'),
-(4, 2, 1, 2, 50, '2018-10-26 14:10:28', '2018-10-26 14:10:28');
+INSERT INTO `order_products` (`id`, `order_id`, `product_id`, `quantity`, `unit_price`) VALUES
+(2, 1, 1, 1, 57.08),
+(3, 1, 2, 3, 46.22),
+(4, 2, 1, 2, 50);
 
 -- --------------------------------------------------------
 
@@ -124,8 +116,6 @@ CREATE TABLE IF NOT EXISTS `products` (
   `description` longtext COLLATE utf8_unicode_ci,
   `unit_price` double DEFAULT NULL,
   `range_id` int(10) UNSIGNED DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `url_photo` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_product_range` (`range_id`)
@@ -135,35 +125,9 @@ CREATE TABLE IF NOT EXISTS `products` (
 -- Déchargement des données de la table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `description`, `unit_price`, `range_id`, `created_at`, `updated_at`, `url_photo`) VALUES
-(1, 'Honor 6C', 'Le Honor 6C est le nouveau smartphone entrée de gamme de la marque \"jeune et branchée\" de Huawei. Il est équipé d\'un écran HD de 5 pouces et d\'un processeur Snapdragon 435 à 1,4 Ghz avec 3 Go de RAM et 32 Go de stockage. Son capteur photo est de 13 mégapixels.', 499, 2, '2018-10-26 14:10:27', '2018-10-26 14:10:27', 'https://www.google.fr/url?sa=i&source=images&cd=&ved=2ahUKEwjarsiXmaTeAhVSSxoKHSw4DecQjRx6BAgBEAU&url=https%3A%2F%2Fwww.frandroid.com%2Fproduits%2Fhonor%2Fsmartphones%2F6422-honor-6c&psig=AOvVaw03xm-VWMYtcpwQdTbt5Cid&ust=1540646377796009'),
-(2, 'Iphone 6S', 'Neuvième génération d’iPhone, l’iPhone 6S a été présenté par Apple le 12 septembre 2015 à l’occasion de sa traditionnelle keynote de rentrée. Ce nouvel iPhone, qui, d’apparence, ressemble beaucoup à son prédécesseur, bénéficie de multiples améliorations techniques, aussi bien dans les composants de sa plate-forme que dans le châssis sur lequel il s’appuie. Certainement en référence à la polémique du Xperia Z3+ de Sony que certains observateurs estimaient trop proche de son prédécesseur, Apple a justement axé sa communication autour des changements qui ne se voient pas : « Une seule chose a changé. Tout. »\r\n', 599, 3, '2018-10-26 14:10:27', '2018-10-26 14:10:27', 'https://www.google.fr/url?sa=i&source=images&cd=&ved=2ahUKEwjbwJ_XmaTeAhVNyhoKHTyjBm8QjRx6BAgBEAU&url=https%3A%2F%2Fswitch.com.my%2Fiphone-6s-plus%2F&psig=AOvVaw2bkcqNqOr_jhk1hILg_3Ar&ust=1540646517449647');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `ranges`
---
-
-DROP TABLE IF EXISTS `ranges`;
-CREATE TABLE IF NOT EXISTS `ranges` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `parent_id` int(10) UNSIGNED DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `IDX_range_parent` (`parent_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Déchargement des données de la table `ranges`
---
-
-INSERT INTO `ranges` (`id`, `name`, `parent_id`, `created_at`, `updated_at`) VALUES
-(2, 'Main range', NULL, '2018-10-26 14:10:27', '2018-10-26 14:10:27'),
-(3, 'Second range', 1, '2018-10-26 14:10:27', '2018-10-26 14:10:27'),
-(4, 'Third range', 1, '2018-10-26 14:10:27', '2018-10-26 14:10:27');
+INSERT INTO `products` (`id`, `name`, `description`, `unit_price`, `range_id`, `url_photo`) VALUES
+(1, 'Honor 6C', 'Le Honor 6C est le nouveau smartphone entrée de gamme de la marque \"jeune et branchée\" de Huawei. Il est équipé d\'un écran HD de 5 pouces et d\'un processeur Snapdragon 435 à 1,4 Ghz avec 3 Go de RAM et 32 Go de stockage. Son capteur photo est de 13 mégapixels.', 499, 2, 'https://bestengine.humanoid.fr/uploads/products/honor-6c.png'),
+(2, 'Iphone 6S', 'Neuvième génération d’iPhone, l’iPhone 6S a été présenté par Apple le 12 septembre 2015 à l’occasion de sa traditionnelle keynote de rentrée. Ce nouvel iPhone, qui, d’apparence, ressemble beaucoup à son prédécesseur, bénéficie de multiples améliorations techniques, aussi bien dans les composants de sa plate-forme que dans le châssis sur lequel il s’appuie. Certainement en référence à la polémique du Xperia Z3+ de Sony que certains observateurs estimaient trop proche de son prédécesseur, Apple a justement axé sa communication autour des changements qui ne se voient pas : « Une seule chose a changé. Tout. »\r\n', 599, 3, 'http://pluspng.com/img-png/iphone-6s-png-apple-iphone-6s-plus-430.png');
 
 -- --------------------------------------------------------
 
@@ -179,8 +143,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `billing_adress_id` int(10) UNSIGNED DEFAULT NULL,
   `delivery_adress_id` int(10) UNSIGNED DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `IDX_user_billing_adress` (`billing_adress_id`),
   KEY `IDX_user_delivery_adress` (`delivery_adress_id`)
@@ -190,8 +152,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `billing_adress_id`, `delivery_adress_id`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@gmail.com', 'admin', 1, 2, '2018-10-26 14:10:28', '2018-10-26 14:10:28');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `billing_adress_id`, `delivery_adress_id`) VALUES
+(1, 'admin', 'admin@gmail.com', 'admin', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -208,8 +170,6 @@ CREATE TABLE IF NOT EXISTS `user_addresses` (
   `postal_code` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `city` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `country` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -217,8 +177,8 @@ CREATE TABLE IF NOT EXISTS `user_addresses` (
 -- Déchargement des données de la table `user_addresses`
 --
 
-INSERT INTO `user_addresses` (`id`, `human_name`, `address_one`, `address_two`, `postal_code`, `city`, `country`, `created_at`, `updated_at`) VALUES
-(1, 'admin', '41 boulevard Vauban', '3eme étage', '59000', 'Lille', 'FRANCE', '2018-10-26 14:10:27', '2018-10-26 14:10:27');
+INSERT INTO `user_addresses` (`id`, `human_name`, `address_one`, `address_two`, `postal_code`, `city`, `country`) VALUES
+(1, 'admin', '41 boulevard Vauban', '3eme étage', '59000', 'Lille', 'FRANCE');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
